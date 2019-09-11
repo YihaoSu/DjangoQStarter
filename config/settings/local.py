0f54e6e443646487b0ba94e7c98deefaa326903c
YihaoSu <yhsu@astro.ncu.edu.tw>
2019-09-11 21:55:43 +0800

    Separate local and production settings

---
+++ config/settings/local.py
@@ -0,0 +1,28 @@
+from .base import *  # noqa
+from .base import env
+
+# SECRET CONFIGURATION
+# ------------------------------------------------------------------------------
+# See: https://docs.djangoproject.com/en/2.2/ref/settings/#secret-key
+# Note: This key only used for development and testing.
+SECRET_KEY = env('DJANGO_SECRET_KEY', default='^385m+!ah4-@av1w4asadfs+hgd+dw8ov!$kbs99=00sdfubt+70m')
+
+# DEBUG
+# ------------------------------------------------------------------------------
+# See: https://docs.djangoproject.com/en/2.2/ref/settings/#debug
+DEBUG = True
+
+# ALLOWED_HOSTS
+# See: https://docs.djangoproject.com/en/2.2/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = [
+    'localhost',
+    '0.0.0.0',
+    '127.0.0.1',
+]
+
+# django-debug-toolbar
+# ------------------------------------------------------------------------------
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
+INSTALLED_APPS += ['debug_toolbar']  # noqa F405
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']  # noqa F405
---
+++ config/settings/production.py
@@ -0,0 +1,25 @@
+from .base import *  # noqa
+from .base import env 
+
+# SITE CONFIGURATION
+# ------------------------------------------------------------------------------
+# Hosts/domain names that are valid for this site
+# See https://docs.djangoproject.com/en/2.2/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=[])
+# END SITE CONFIGURATION
+
+# SECRET CONFIGURATION
+# ------------------------------------------------------------------------------
+# See: https://docs.djangoproject.com/en/2.2/ref/settings/#secret-key
+# Raises ImproperlyConfigured exception if DJANGO_SECRET_KEY not in os.environ
+SECRET_KEY = env('DJANGO_SECRET_KEY')
+# END SECRET CONFIGURATION
+
+# DATABASE CONFIGURATION
+# ------------------------------------------------------------------------------
+# Use the Heroku-style specification
+# Raises ImproperlyConfigured exception if DATABASE_URL not in os.environ
+DATABASES['default'] = env.db('DATABASE_URL')  # noqa F405
+DATABASES['default']['ATOMIC_REQUESTS'] = True  # noqa F405
+DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=60)  # noqa F405
+# END DATABASE CONFIGURATION
